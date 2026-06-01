3849675aab72f0bc474ad85637716633591b6644
vinubussiness-cyber <vinubussiness@gmail.com>
2026-06-02 00:44:45 +0530

    Add files via upload

--- api/contact.js
+++ api/contact.js
@@ -5,6 +5,8 @@ module.exports = async (req, res) => {
     return res.status(405).json({ message: "Method not allowed" });
   }
 
+  console.log(req.body)
+
   try {
     const { name, email, phone, company, service, description } = req.body;
 
@@ -32,6 +34,7 @@ module.exports = async (req, res) => {
     });
 
     return res.status(200).json({ success: true });
+
   } catch (error) {
     console.error("Email error:", error);
     return res.status(500).json({
